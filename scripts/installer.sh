a4a52b00abd0bb45083d8314a73f359212cd3a98
Aarav619 <aaravmishra619@gmail.com>
2022-01-26 01:02:57 +0530

    Add install script to install packages & setup

---
+++ scripts/installer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Requires sudo
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+        exit
+fi
+
+
+echo -e "\n======> Installing AM-OS.\n"
+
+
+pacman -Sy --needed $(<pkg/packages.txt)
+echo -e "\n======> Completed installing packages."
+
+# Copying config files to ~/.config/ dir
+cp -frv \
+	../alacritty \
+	../mako \
+	../sway \
+	../dunst \
+	../swaylock \
+	../waybar \
+	../wofi \
+	../../.config/
+
+echo -e "\n======> Placed configs to thier respective dir..."
+
+echo -e "\nAM-OS installed... Reboot now for changes to take effect!"
