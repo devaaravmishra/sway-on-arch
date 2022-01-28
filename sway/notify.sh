7ed2ae4a357b20d83df113de55f211ca87c5c3a0
Aarav619 <aaravmishra619@gmail.com>
2022-01-29 01:09:35 +0530

    Add vol progress bar via dunstify
    This script enables volume progress bar via dunstify

---
+++ sway/notify.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+
+notifyMuted() {
+    volume="$1"
+    icon="~/.config/sway/mute.png"
+    dunstify -h string:x-canonical-private-synchronous:audio "Muted" -h int:value:"$volume" -t 1500 --icon $icon
+}
+
+notifyAudio() {
+    volume="$1"
+    ponymix is-muted && notifyMuted "$volume" && return
+    
+    if [ $volume -eq 0 ]; then
+        notifyMuted "$volume"
+        elif [ $volume -le 30 ]; then
+        icon="~/.config/sway/low.png"
+        dunstify    -h string:x-canonical-private-synchronous:audio "Volume: "$volume%"" -h int:value:"$volume" -t 1500 --icon $icon
+        elif [ $volume -le 70 ]; then
+        icon="~/.config/sway/high.png"
+        dunstify -h string:x-canonical-private-synchronous:audio "Volume: "$volume%"" -h int:value:"$volume" -t 1500 --icon $icon
+    else
+        icon="~/.config/sway/high.png"
+        dunstify -h string:x-canonical-private-synchronous:audio "Volume: "$volume%"" -h int:value:"$volume" -t 1500 --icon $icon
+    fi
+}
+
+input=`cat /dev/stdin`
+
+case "$1" in
+    muted)
+        volume=`ponymix get-volume`
+        if [ "$input" -eq 0 ]
+        then
+            notifyAudio "$volume"
+        else
+            notifyMuted "$volume"
+        fi
+    ;;
+    audio)
+        notifyAudio "$input"
+    ;;
+    brightness)
+        notifyBrightness "$input"
+    ;;
+    
+    *)
+        echo "Not the right arguments"
+        echo "$1"
+        exit 2
+esac
