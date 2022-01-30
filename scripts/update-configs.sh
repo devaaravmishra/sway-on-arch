04e2b268a8f4ab9bc966ada24661a01af2cca6db
Aarav619 <aaravmishra619@gmail.com>
2022-01-30 22:48:20 +0530

    Add script to update local changes

---
+++ scripts/update-configs.sh
@@ -0,0 +1,10 @@
+# Sync dotfiles with git repo
+
+cp -rv \
+    ${HOME}/.config/alacritty \
+    ${HOME}/.config/dunst \
+    ${HOME}/.config/sway \
+    ${HOME}/.config/swaylock \
+    ${HOME}/.config/waybar \
+    ${HOME}/.config/wofi \
+    ../
