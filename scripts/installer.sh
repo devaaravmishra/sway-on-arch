99b7fcba6bfcda5127a1043c0f8a2b0c15ecbfdf
Aarav619 <aaravmishra619@gmail.com>
2022-02-10 14:34:48 +0530

    Fix paths & print statements

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -7,14 +7,14 @@ if [ "$EUID" -ne 0 ]
 fi
 
 
-echo -e "\n======> Installing AM-OS.\n"
+echo -e "\n======> Installing Sway-On-Arch.\n"
 
 
-pacman -Sy --needed $(<pkg/packages.txt)
+pacman -Sy $(<packages.txt)
+yay -S swaylock-effects
 echo -e "\n======> Completed installing packages."
 
 # Copying config files to ~/.config/ dir
-mkdir -p ${HOME}/.config
 cp -frv \
 	../alacritty \
 	../sway \
@@ -22,14 +22,16 @@ cp -frv \
 	../swaylock \
 	../waybar \
 	../wofi \
-	../../.config/
+	${HOME}/.config/
 
 echo -e "\n======> Done Patching configs..."
 
 echo -e "\n======> Setting Wallpaper for home & lockscreen..."
 
+mkdir -p ${HOME}/Pictures
+
 cp -frv \
 	../Wallpapers \
-	../../Pictures/
+	${HOME}/Pictures/
 
 echo -e "\nSway On Arch installed... Reboot now for changes to take effect!"
