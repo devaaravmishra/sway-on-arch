609cb2dbeac702a090ae918b53851683a92ff6d7
Aarav619 <aaravmishra619@gmail.com>
2022-03-16 21:17:31 +0530

    mv configs to .config dir
    cleaning repo ;)

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-# Requires sudo
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
-        exit
-fi
-
-
 echo -e "\n======> Installing Sway-On-Arch.\n"
 
 
-pacman -Sy $(<packages.txt)
-yay -S swaylock-effects
-echo -e "\n======> Completed installing packages."
-
 # Copying config files to ~/.config/ dir
-cp -frv \
-	../alacritty \
-	../sway \
-	../dunst \
-	../swaylock \
-	../waybar \
-	../wofi \
-	${HOME}/.config/
+cp -frv ../.config/ \
+${HOME}/
 
 echo -e "\n======> Done Patching configs..."
 
-echo -e "\n======> Setting Wallpaper for home & lockscreen..."
+echo -e "\n======> Setting Wallpaper!"
 
 mkdir -p ${HOME}/Pictures
 
