1aa855899d77c2b222db95ffb54c0401f4143352
Aarav619 <aaravmishra619@gmail.com>
2022-03-25 14:56:11 +0530

    reconfigure geometry size for dialog box

--- .config/sway/scripts/swaykb.sh
+++ .config/sway/scripts/swaykb.sh
@@ -4,6 +4,6 @@ sed -n '/Key bindings/,/Ctrl+Shift+r/p' ~/.config/sway/config | \
 grep '#\|##\|bindsym\|^$' | \
 sed -e 's/# //' -e 's/#//' -e 's/##//' -e 's/###//' -e 's/^[ \t]*//' | \
 grep -v '#Key bindings' | \
-yad --text-info --fore=#46d9ff --geometry=1200x800
+yad --text-info --fore=#46d9ff --geometry=800x600
 
 
