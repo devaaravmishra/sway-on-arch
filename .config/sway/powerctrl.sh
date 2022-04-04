55916c0e3e924b43d2e3ffc53a9b5fdced188359
Aarav619 <aaravmishra619@gmail.com>
2022-04-04 22:05:01 +0530

    Rm clutter from sway dir

--- .config/sway/powerctrl.sh
+++
@@ -1,1 +0,0 @@
-/home/am/.config/wofi/powerctrl.sh
--- .config/sway/scripts/swaykb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sed -n '/Key bindings/,/Ctrl+Shift+r/p' ~/.config/sway/config | \
-grep '#\|##\|bindsym\|^$' | \
-sed -e 's/# //' -e 's/#//' -e 's/##//' -e 's/###//' -e 's/^[ \t]*//' | \
-grep -v '#Key bindings' | \
-yad --text-info --fore=#46d9ff --geometry=800x600
-
-
