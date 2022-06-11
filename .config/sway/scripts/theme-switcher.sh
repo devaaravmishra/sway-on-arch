28e3e7b426d2cea5674fc53d4ae5be39461d5e40
Aarav619 <aaravmishra619@gmail.com>
2022-06-11 11:25:20 +0530

    Now patches sway theme too!

--- .config/sway/scripts/theme-switcher.sh
+++ .config/sway/scripts/theme-switcher.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #!/usr/bin/env bash
-current_theme="$(grep '@import' ~/.config/waybar/style.css | cut -d'/' -f2- | cut -d'.' -f1)"
+current_waybar_theme="$(grep '@import' ~/.config/waybar/style.css | cut -d'/' -f2- | cut -d'.' -f1)"
+current_sway_theme="$(grep 'colorschemes' ~/.config/sway/config | cut -d'/' -f2- | cut -d'/' -f4 | cut -d'.' -f1)"
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     echo -e "\n\t--This script can switch color schemes with given choice of colors your configs contains."
@@ -16,15 +17,16 @@ if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     exit 0;
 fi
 
-selected="$(ls ~/.config/waybar/colorschemes |  cut -d' ' -f11- | cut -d'.' -f1 | fzf --prompt " (applied: $current_theme) select theme:  ")"
+selected="$(ls ~/.config/waybar/colorschemes |  cut -d' ' -f11- | cut -d'.' -f1 | fzf --prompt " (applied: $current_waybar_theme) select theme:  ")"
 
 if [ "$selected" == "" ];
 then
 	echo 'Selection terminated, No theme applied!'
 	exit 0;
 else
-	sed -i "s/${current_theme}/${selected}/" ~/.config/waybar/style.css && sway reload && \
+	sed -i "s/${current_waybar_theme}/${selected}/" ~/.config/waybar/style.css && \
 	sed -i "/colors:/c\colors: *${selected}" ~/.config/alacritty/alacritty.yml && \
+	sed -i "s/${current_sway_theme}/${selected}/" ~/.config/sway/config && sway reload && \
 	notify-send "${selected^} theme applied!"
 	exit 0;
 fi
